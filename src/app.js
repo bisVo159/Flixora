f8b62703dd686e62e50376646c00253e7d52cbe1
bisVo159 <biswasanik159@gmail.com>
2025-09-25 18:52:20 +0530

    add healthcheck route and controller for API health status

--- src/app.js
+++ src/app.js
@@ -13,10 +13,12 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 // routes
-import userRoutes from "./routes/user.routes.js";
-import videoRoutes from "./routes/video.routers.js";
+import healthcheckRouter from "./routes/healthcheck.routes.js";
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/video.routers.js";
 
-app.use("/api/v1/users", userRoutes);
-app.use("/api/v1/videos", videoRoutes);
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/videos", videoRouter);
 
 export {app};
