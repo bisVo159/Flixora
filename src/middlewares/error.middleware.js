ca1c3681435c8ab7fffa646dea53b0bcc6975120
bisVo159 <biswasanik159@gmail.com>
2025-09-26 12:08:36 +0530

    add error middleware for centralized error handling

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,15 @@
+import { ApiError } from "../utils/ApiError";
+
+export const errorMiddleware = (err, req, res, next) => {
+    let customError = err;
+    if(!(err instanceof ApiError)){
+        customError = new ApiError(err?.code || 500, err?.message || "Internal Server Error");
+    }
+    return res.status(customError.statusCode).json({
+        success: false,
+        message: customError.message || "Something went wrong",
+        errors: customError.errors || [],
+        data: customError.data || null,
+        stack: process.env.NODE_ENV === "development" ? customError.stack : undefined
+    });
+}
